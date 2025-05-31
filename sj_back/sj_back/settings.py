d48b377ffa9a41708ca9f995678a9963744bf590
nikita <erm.nikita1212@gmail.com>
2025-05-31 15:57:44 +0300

    fixed lib jazzmin

--- sj_back/sj_back/settings.py
+++ sj_back/sj_back/settings.py
@@ -42,6 +42,58 @@ INSTALLED_APPS = [
     "app",
 ]
 
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+JAZZMIN_SETTINGS = {
+    "show_ui_builder": True,
+    "site_brand": "Student's Job admin",
+    "site_icon": "img/logo.png",
+    "site_logo": "img/logo.png",
+    "site_logo_classes": "img-circle",
+    "login_logo": "img/logo.png",
+    "login_logo_dark": "img/logo.png",
+}
+
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-indigo",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-indigo",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
 # Remove all channel layers configuration
 # No need to keep the commented out code
 
@@ -126,16 +178,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
